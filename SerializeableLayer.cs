88f21e7d9601bf36092c970b18fad134e122a3e2
Paul Poinet <poinetp@gmail.com>
2017-08-09 15:28:45 +0200

    Add files via upload

---
+++ SerializeableLayer.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Rhino;
+
+namespace LayerStalker
+{
+    public class SerializeableLayer
+    {
+        [JsonProperty("id")]
+        public Guid Uuid { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("parent")]
+        public Guid? ParentId { get; set; }
+
+        public int LayerIndex { get; set; }
+
+        public string LayerColor { get; set; }
+
+        public bool IsExp { get; set; }
+
+        public bool IsLocked { get; set; }
+
+        public bool IsVisible { get; set; }
+
+        public int Size { get; set; }
+
+        public string CurrLayer { get; set; }
+
+        public List<string> Predecessors { get; set; }
+
+        public Rhino.DocObjects.RhinoObject[] Objects { get; set; }
+
+        public SerializeableLayer() { }
+    }
+}
